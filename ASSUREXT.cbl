8fba8ce8b05ddfb4c1826aeed56a99f4e3a49b18
tmnpierre <tomyn.pierre@gmail.com>
2024-04-15 10:55:00 +0200

    initial commit

--- ASSUREXT.cbl
+++ ASSUREXT.cbl
@@ -35,7 +35,6 @@
                ACCESS MODE IS SEQUENTIAL
                FILE STATUS IS WS-FILE-STATUS.
 
-
        DATA DIVISION.
        FILE SECTION.
        FD  FICHIER-ASSUR-PART1.
@@ -73,196 +72,164 @@
                05  ASSUR-DATE-DEB2     PIC X(8).
                05  FILLER              PIC X(1).
                05  ASSUR-DATE-FIN2     PIC X(8).
-               05  FILLER              PIC X(1).
-               05  ASSUR-MONTANT2      PIC X(9).
-               05  FILLER              PIC X(1).
-               05  ASSUR-DEVISE2       PIC X(3).
+               05 FILLER               PIC X(1).
+               05 ASSUR-MONTANT2       PIC X(9).
+               05 FILLER               PIC X(1).
+               05 ASSUR-DEVISE2        PIC X(3).
 
        FD  FICHIER-RAPPORT.
            01  RAPPORT-REC.
-      *        Longueur du segment de données.
                05  WS-RAPPORT-LENGTH  PIC 9(3).
-      *        Les données du rapport.
                05  WS-RAPPORT-DATA    PIC X(147).
 
-       WORKING-STORAGE SECTION.
-       
-      *    Variable pour stocker le code de statut de l'opération
-      *    de fichier.
+             WORKING-STORAGE SECTION.
        01  WS-FILE-STATUS        PIC XX.
-
-      *    Date et heure actuelles.
        01  WS-CURRENT-TIME       PIC X(20).
-
-      *    Compteur pour suivre le nombre d'enregistrements lus.
        01  WS-REC-COUNT          PIC 9(4) VALUE ZERO.
-
-      *    Total des enregistrements actifs.
        01  WS-TOTAL-ACTIF        PIC 9(4) VALUE ZERO.
-
-      *    Total des enregistrements inactifs.
-       01  WS-TOTAL-INACTIF      PIC 9(4) VALUE ZERO.
-
-      *    Total des enregistrements lus.
+       01  WS-TOTAL-RESILIE      PIC 9(4) VALUE ZERO.
+       01  WS-TOTAL-SUSPENDU     PIC 9(4) VALUE ZERO.
        01  WS-TOTAL-RECORDS      PIC 9(6) VALUE ZERO.
-
-      *    Informations de pied de page du rapport.
        01  WS-FOOTER-INFO        PIC X(147).
+       01  WS-BLANK-LINE         PIC X(147) VALUE SPACES.
 
-      *    Ligne vide pour le rapport.
-       01  WS-BLANK-LINE          PIC X(147) VALUE SPACES.
+       01  ACTIVE-RECORDS.
+           05  AR-RECORDS OCCURS 500 TIMES INDEXED BY AR-IDX.
+               10  AR-DATA     PIC X(147).
+       01  RESILIE-RECORDS.
+           05  RS-RECORDS OCCURS 500 TIMES INDEXED BY RS-IDX.
+               10  RS-DATA     PIC X(147).
+       01  SUSPENDU-RECORDS.
+           05  SP-RECORDS OCCURS 500 TIMES INDEXED BY SP-IDX.
+               10  SP-DATA     PIC X(147).
 
        PROCEDURE DIVISION.
-
-      *    Ouverture des fichiers d'entrée et de sortie.
+       MAIN-LOGIC.
            OPEN INPUT FICHIER-ASSUR-PART1 FICHIER-ASSUR-PART2
                 OUTPUT FICHIER-RAPPORT.
                 
-      *    Écriture du titre du rapport.
            MOVE "Rapport Global des Assurances" TO WS-RAPPORT-DATA
            MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
            WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
            WRITE RAPPORT-REC FROM WS-BLANK-LINE
            WRITE RAPPORT-REC FROM WS-BLANK-LINE.
 
-      *    Écriture de la section des statuts et contrats.
-           MOVE "Section: Statuts et Contrats" TO WS-RAPPORT-DATA
-           MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
-           WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
-           WRITE RAPPORT-REC FROM WS-BLANK-LINE
-           WRITE RAPPORT-REC FROM WS-BLANK-LINE.
-
-      *    Initialisation du code de statut de fichier.
            MOVE ZERO TO WS-FILE-STATUS
-           
-      *    Traitement du premier fichier d'assurance.
            PERFORM PROCESS-FILE-1
-           CLOSE FICHIER-ASSUR-PART1
-
-      *    Réinitialisation du code de statut de fichier.
-           MOVE ZERO TO WS-FILE-STATUS
-           
-      *    Traitement du deuxième fichier d'assurance.
            PERFORM PROCESS-FILE-2
-           CLOSE FICHIER-ASSUR-PART2
 
-      *    Génération du résumé des enregistrements.
            STRING "Total Records: " WS-TOTAL-RECORDS 
                  DELIMITED BY SIZE
-                " Total Actif: " WS-TOTAL-ACTIF 
-                 DELIMITED BY SIZE
-                " Total Inactif: " WS-TOTAL-INACTIF 
+                 " Actifs: " WS-TOTAL-ACTIF 
+                 " Résiliés: " WS-TOTAL-RESILIE 
+                 " Suspendus: " WS-TOTAL-SUSPENDU 
                  DELIMITED BY SIZE
                  INTO WS-FOOTER-INFO
            MOVE WS-FOOTER-INFO TO WS-RAPPORT-DATA
            MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
            WRITE RAPPORT-REC FROM WS-RAPPORT-DATA.
+           WRITE RAPPORT-REC FROM WS-BLANK-LINE.
 
-      *    Fermeture du fichier de rapport.
+      * Section pour les actifs
+           MOVE "Enregistrements Actifs:" TO WS-RAPPORT-DATA
+           MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
+           WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
+           WRITE RAPPORT-REC FROM WS-BLANK-LINE
+           PERFORM WRITE-ACTIVE-RECORDS
+
+      * Section pour les résiliés
+           MOVE "Enregistrements Résiliés:" TO WS-RAPPORT-DATA
+           MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
+           WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
+           WRITE RAPPORT-REC FROM WS-BLANK-LINE
+           WRITE RAPPORT-REC FROM WS-BLANK-LINE.
+           PERFORM WRITE-RESILIE-RECORDS
+
+      * Section pour les suspendus
+           MOVE "Enregistrements Suspendus:" TO WS-RAPPORT-DATA
+           MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
+           WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
+           WRITE RAPPORT-REC FROM WS-BLANK-LINE
+           WRITE RAPPORT-REC FROM WS-BLANK-LINE.
+           PERFORM WRITE-SUSPENDU-RECORDS
+
+           CLOSE FICHIER-ASSUR-PART1
+           CLOSE FICHIER-ASSUR-PART2
            CLOSE FICHIER-RAPPORT.
-           
-      *    Affichage d'un message de fin.
            DISPLAY "FIN DE TRAITEMENT DES ENREGISTREMENTS."
-           
-      *    Arrêt de l'exécution du programme.
            STOP RUN.
 
        PROCESS-FILE-1.
-       
-      *    Boucle jusqu'à ce que le code de statut de fichier soit '10',
-      *    indiquant la fin du fichier.
            PERFORM UNTIL WS-FILE-STATUS = '10'
-               
                READ FICHIER-ASSUR-PART1 INTO ASSUR-REC AT END
-               
-      *    Si fin de fichier, mettre à jour le code de statut.
                    MOVE '10' TO WS-FILE-STATUS
-                   
                NOT AT END
-              
-      *    Évaluation du statut de l'assurance.
                    EVALUATE ASSUR-STATUT
-                       
-      *    Si l'assurance est active, incrémenter le total d'assurances
-      *    actives.
                        WHEN 'Actif'
                            ADD 1 TO WS-TOTAL-ACTIF
-                       
-      *    Si l'assurance est inactive, incrémenter le total 
-      *    d'assurances inactives.
+                           MOVE ASSUR-REC TO AR-DATA(WS-TOTAL-ACTIF)
+                       WHEN 'Resilie'
+                           ADD 1 TO WS-TOTAL-RESILIE
+                           MOVE ASSUR-REC TO RS-DATA(WS-TOTAL-RESILIE)
+                       WHEN 'Suspendu'
+                           ADD 1 TO WS-TOTAL-SUSPENDU
+                           MOVE ASSUR-REC TO SP-DATA(WS-TOTAL-SUSPENDU)
                        WHEN OTHER
-                           ADD 1 TO WS-TOTAL-INACTIF
+                           CONTINUE
                    END-EVALUATE
-                   
-      *    Incrémenter le total des enregistrements lus.
                    ADD 1 TO WS-TOTAL-RECORDS
-                   
-      *    Obtenir la date et l'heure actuelles.
-                   MOVE FUNCTION CURRENT-DATE TO WS-CURRENT-TIME
-                   
-      *    Construction de la ligne de rapport.
-                   STRING " ID: " ASSUR-ID
-                          " Assurance souscrite: " ASSUR-NOM 
-                          " Statut: " ASSUR-STATUT DELIMITED BY SIZE
-                          INTO WS-RAPPORT-DATA
-                   MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
-                   
-      *    Écriture de la ligne de rapport dans le fichier de rapport.
-                   WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
-                   WRITE RAPPORT-REC FROM WS-BLANK-LINE
-               
-      *    Lecture de l'enregistrement suivant dans le fichier.
                END-READ
-               
-      *    Fin de la boucle de traitement.
            END-PERFORM.
 
        PROCESS-FILE-2.
-       
-      *    Boucle jusqu'à ce que le code de statut de fichier soit '10',
-      *    indiquant la fin du fichier.
            PERFORM UNTIL WS-FILE-STATUS = '10'
-               
                READ FICHIER-ASSUR-PART2 INTO ASSUR-REC-2 AT END
-               
-      *    Si fin de fichier, mettre à jour le code de statut.
                    MOVE '10' TO WS-FILE-STATUS
-                   
                NOT AT END
-              
-      *    Évaluation du statut de l'assurance.
                    EVALUATE ASSUR-STATUT2
-                       
-      *    Si l'assurance est active, incrémenter le total d'assurances 
-      *    actives.
                        WHEN 'Actif'
                            ADD 1 TO WS-TOTAL-ACTIF
-                       
-      *    Si l'assurance est inactive, incrémenter le total 
-      *    d'assurances inactives.
+                           MOVE ASSUR-REC-2 TO AR-DATA(WS-TOTAL-ACTIF)
+                       WHEN 'Resilie'
+                           ADD 1 TO WS-TOTAL-RESILIE
+                           MOVE ASSUR-REC-2 TO RS-DATA(WS-TOTAL-RESILIE)
+                       WHEN 'Suspendu'
+                           ADD 1 TO WS-TOTAL-SUSPENDU
+                          MOVE ASSUR-REC-2 TO SP-DATA(WS-TOTAL-SUSPENDU)
                        WHEN OTHER
-                           ADD 1 TO WS-TOTAL-INACTIF
+                           CONTINUE
                    END-EVALUATE
-                   
-      *    Incrémenter le total des enregistrements lus.
                    ADD 1 TO WS-TOTAL-RECORDS
-                   
-      *    Obtenir la date et l'heure actuelles.
-                   MOVE FUNCTION CURRENT-DATE TO WS-CURRENT-TIME
-                   
-      *    Construction de la ligne de rapport.
-                   STRING " ID: " ASSUR-ID2
-                          " Assurance souscrite: " ASSUR-NOM2 
-                          " Statut: " ASSUR-STATUT2 DELIMITED BY SIZE
-                          INTO WS-RAPPORT-DATA
-                   MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
-                   
-      *    Écriture de la ligne de rapport dans le fichier de rapport.
-                   WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
-                   WRITE RAPPORT-REC FROM WS-BLANK-LINE
-               
-      *    Lecture de l'enregistrement suivant dans le fichier.
                END-READ
-               
-      *    Fin de la boucle de traitement.
+           END-PERFORM.
+
+
+       WRITE-ACTIVE-RECORDS.
+           PERFORM VARYING AR-IDX FROM 1 BY 1 
+                   UNTIL AR-IDX > WS-TOTAL-ACTIF
+               MOVE AR-DATA(AR-IDX OF ACTIVE-RECORDS) TO 
+               WS-RAPPORT-DATA
+               MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
+               WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
+               WRITE RAPPORT-REC FROM WS-BLANK-LINE
+           END-PERFORM.
+
+       WRITE-RESILIE-RECORDS.
+           PERFORM VARYING RS-IDX FROM 1 BY 1 
+                   UNTIL RS-IDX > WS-TOTAL-RESILIE
+               MOVE RS-DATA(RS-IDX OF RESILIE-RECORDS) TO 
+               WS-RAPPORT-DATA
+               MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
+               WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
+               WRITE RAPPORT-REC FROM WS-BLANK-LINE
+           END-PERFORM.
+
+       WRITE-SUSPENDU-RECORDS.
+           PERFORM VARYING SP-IDX FROM 1 BY 1 
+                   UNTIL SP-IDX > WS-TOTAL-SUSPENDU
+               MOVE SP-DATA(SP-IDX OF SUSPENDU-RECORDS) TO 
+               WS-RAPPORT-DATA
+               MOVE LENGTH OF WS-RAPPORT-DATA TO WS-RAPPORT-LENGTH
+               WRITE RAPPORT-REC FROM WS-RAPPORT-DATA
+               WRITE RAPPORT-REC FROM WS-BLANK-LINE
            END-PERFORM.
